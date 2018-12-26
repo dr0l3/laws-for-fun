3530e9d71784b52d43e4ee4f36bbc0fb33aee5bd
Rune Pedersen <rdp@dixa.com>
2018-12-26 17:49:09 +0100

    Eureka

---
+++ server/src/main/scala/SomethingNice.scala
@@ -0,0 +1,46 @@
+import java.util.UUID
+
+import cats.Id
+
+import scala.collection.mutable.ListBuffer
+
+case class Something(id: UUID, name: String, age: Int, likesIceCream: Boolean)
+
+trait SomethingNice[F[_]] {
+  def save(s: Something): F[Option[Something]]
+  def findById(id: UUID): F[Option[Something]]
+  def findByName(name: String): F[List[Something]]
+  def exists(id: UUID): F[Boolean]
+  def delete(s: Something): F[Option[Something]]
+}
+
+class SomethingNiceInRealitity(initialElements: List[Something] = Nil) extends SomethingNice[Id] {
+  val list = new ListBuffer[Something]() ++ initialElements
+
+  override def save(s: Something): Id[Option[Something]] = {
+    list.append(s)
+    Option(s)
+  }
+
+  override def findById(id: UUID): Id[Option[Something]] = {
+    list.find(_.id == id)
+  }
+
+  override def findByName(name: String): Id[List[Something]] = {
+    list.filter(_.name == name).toList
+  }
+
+  override def exists(id: UUID): Id[Boolean] = {
+    list.exists(_.id == id)
+  }
+
+  override def delete(s: Something): Id[Option[Something]] = {
+    val index = list.indexOf(s)
+    if (index > -1) {
+      list.remove(index)
+      Option(s)
+    } else {
+      None
+    }
+  }
+}
---
+++ server/src/test/scala/SomethingNiceLaws.scala
@@ -0,0 +1,60 @@
+import cats.Monad
+import cats.kernel.laws._
+import org.scalatest.FunSuite
+import org.typelevel.discipline.scalatest.Discipline
+
+trait SomethingNiceLaws[F[_]] {
+  def algebra: SomethingNice[F]
+  implicit def M: Monad[F]
+
+  import cats.syntax.apply._
+  import cats.syntax.flatMap._
+  import cats.syntax.functor._
+
+  def saveFind(s: Something): IsEq[F[Option[Something]]] = {
+    algebra.save(s) >> algebra.findById(s.id) <-> M.pure(Option(s))
+  }
+}
+
+object SomethingNiceLaws {
+  def apply[F[_]](instance: SomethingNice[F])(implicit ev: Monad[F]) =
+    new SomethingNiceLaws[F] {
+      override val algebra = instance
+      override implicit val M: Monad[F] = ev
+    }
+}
+
+import org.typelevel.discipline.Laws
+import cats.kernel.laws.discipline._
+import cats.{ Eq, Monad }
+import org.scalacheck.Arbitrary
+import org.scalacheck.Prop._
+
+trait EmailAlgebraTests[F[_]] extends Laws {
+  def laws: SomethingNiceLaws[F]
+
+  def algebra(implicit
+    arbEmail: Arbitrary[Something],
+    eqFOptEmail: Eq[F[Option[Something]]]) =
+    new SimpleRuleSet(
+      name = "Emails",
+      "find and save compose" -> forAll(laws.saveFind _))
+}
+
+object EmailAlgebraTests {
+
+  def apply[F[_]: Monad](instance: SomethingNice[F]) = new EmailAlgebraTests[F] {
+    override val laws: SomethingNiceLaws[F] = SomethingNiceLaws(instance)
+  }
+}
+
+import org.scalacheck.ScalacheckShapeless._
+import cats.implicits._
+
+class EmailRepositorySpecs extends FunSuite with Discipline {
+  implicit val uuidArb = Arbitrary.arbUuid
+  implicit val somethingArb = implicitly[Arbitrary[Something]]
+  implicit val eqFoo: Eq[Something] = Eq.fromUniversalEquals
+
+  checkAll("EmailRepository", EmailAlgebraTests(new SomethingNiceInRealitity(Nil)).algebra)
+}
