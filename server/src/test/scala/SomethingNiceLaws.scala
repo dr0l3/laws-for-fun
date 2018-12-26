44febe0b01465e68172b7a5f47168b75875e46b0
Rune Pedersen <rdp@dixa.com>
2018-12-26 18:13:24 +0100

    Additional laws

--- server/src/test/scala/SomethingNiceLaws.scala
+++ server/src/test/scala/SomethingNiceLaws.scala
@@ -2,6 +2,9 @@ import cats.Monad
 import cats.kernel.laws._
 import org.scalatest.FunSuite
 import org.typelevel.discipline.scalatest.Discipline
+import cats.instances.list._
+import cats.instances.option._
+import cats.syntax.traverse._
 
 trait SomethingNiceLaws[F[_]] {
   def algebra: SomethingNice[F]
@@ -13,6 +16,37 @@ trait SomethingNiceLaws[F[_]] {
 
   def saveFind(s: Something): IsEq[F[Option[Something]]] = {
     algebra.save(s) >> algebra.findById(s.id) <-> M.pure(Option(s))
+  }
+
+  def saveFind2(s: Something): IsEq[F[Option[Something]]] = {
+    val prog: F[Option[Something]] = for {
+      _ <- algebra.save(s)
+      right <- algebra.findById(s.id)
+    } yield right
+
+    IsEq(prog, M.pure(Option(s)))
+  }
+
+  def findsAreConsistent(s: List[Something]): IsEq[F[Option[Something]]] = {
+    val findId: F[Option[Something]] = for {
+      _ <- s.traverse { item => algebra.save(item) }
+      found <- s match {
+        case Nil => M.pure(None)
+        case ::(head, _) =>
+          algebra.findById(head.id)
+      }
+    } yield found
+
+    val findName: F[Option[Something]] = for {
+      _ <- s.traverse { item => algebra.save(item) }
+      found <- s match {
+        case Nil => M.pure(List.empty[Something])
+        case ::(head, _) =>
+          algebra.findByName(head.name)
+      }
+    } yield found.find(e => s.headOption.exists(_.id == e.id))
+
+    IsEq(findId, findName)
   }
 }
 
@@ -38,7 +72,9 @@ trait EmailAlgebraTests[F[_]] extends Laws {
     eqFOptEmail: Eq[F[Option[Something]]]) =
     new SimpleRuleSet(
       name = "Emails",
-      "find and save compose" -> forAll(laws.saveFind _))
+      "find and save compose" -> forAll(laws.saveFind _),
+      "find and save compose 2" -> forAll(laws.saveFind2(_)),
+      "Finds are consistent" -> forAll(laws.findsAreConsistent(_)))
 }
 
 object EmailAlgebraTests {
@@ -54,7 +90,7 @@ import cats.implicits._
 class EmailRepositorySpecs extends FunSuite with Discipline {
   implicit val uuidArb = Arbitrary.arbUuid
   implicit val somethingArb = implicitly[Arbitrary[Something]]
-  implicit val eqFoo: Eq[Something] = Eq.fromUniversalEquals
+  implicit val eqFoo: Eq[Option[Something]] = Eq.fromUniversalEquals
 
   checkAll("EmailRepository", EmailAlgebraTests(new SomethingNiceInRealitity(Nil)).algebra)
 }
